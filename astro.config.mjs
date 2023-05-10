f5e40bb4d7ce5be09d41ec5b156a0d49a6ba97ed
Gareth Weaver <reg@garethweaver.com>
2023-05-10 13:01:18 +0100

    tmp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config'
-import prefetch from '@astrojs/prefetch'
+import { defineConfig } from 'astro/config';
+import vercelStatic from '@astrojs/vercel/static';
+import prefetch from '@astrojs/prefetch';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercelStatic(),
   integrations: [
-    prefetch({ throttle: 3 }),
+    prefetch({
+      throttle: 3
+    }),
     react(),
-  ],
+  ]
 });
