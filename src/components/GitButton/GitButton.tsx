5415cb755a3c550589d1c5f6666c0f3d5dc7f3ed
Gareth Weaver <reg@garethweaver.com>
2023-09-11 10:44:46 +0100

    fix(GitButton types): allow nodelist | string

--- src/components/GitButton/GitButton.tsx
+++ src/components/GitButton/GitButton.tsx
@@ -2,7 +2,10 @@ import type { ReactElement } from 'react'
 import { isModalOpen } from '@stores/modalStore'
 import styles from './GitButton.module.sass'
 
-export default (props: { className?: string, children: ReactElement }) => {
+export default (props: {
+  className?: string,
+  children: ReactElement | ReactElement[] | string,
+}) => {
   const handleClick = (): void => isModalOpen.set(true)
   return (
     <button
