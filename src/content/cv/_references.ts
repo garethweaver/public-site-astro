06bdc92ff6dc8938bc01751ed9053bf7e3732596
Gareth Weaver <reg@garethweaver.com>
2024-01-16 18:43:03 +1000

    content(cv): jobs, references, skills

---
+++ src/content/cv/_references.ts
@@ -0,0 +1,33 @@
+interface Reference {
+  name: string
+  role: string
+  email: string
+  company: string
+}
+
+export const referenceList: Reference[] = [
+  {
+    name: 'Rob Watts',
+    role: 'CTO',
+    email: 'robw@wonderbly.com',
+    company: 'Wonderbly',
+  },
+  {
+    name: 'Nick Scott',
+    role: 'Tech Lead',
+    email: 'nick.scott@kortical.com',
+    company: 'Kortical',
+  },
+  {
+    name: 'Liam Walsh',
+    role: 'Creative Tech Director',
+    email: 'liam@liamwalsh.com',
+    company: 'Nexus Studios',
+  },
+  {
+    name: 'Joe Smalley',
+    role: 'Tech Lead',
+    email: 'joesmalley.com',
+    company: 'Breakthrough Media',
+  },
+]
